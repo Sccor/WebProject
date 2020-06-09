e573694184d71e49eae85f20273ed42a80871fe2
Luc MANCEAU <luc.manceau@efrei.net>
2020-06-09 17:04:15 +0200

    header fini partout
    Signed-off-by: Luc MANCEAU <luc.manceau@efrei.net>

--- header.js
+++ header.js
@@ -1,16 +1,15 @@
 var rellax = new Rellax('.rellax', {center: true});
+
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 50) { 
-        document.querySelector('header').className = "fixed"; 
-    // } else if(document.documentElement.scrollTop > 50 && document.querySelector('.nav_right').className.contains('is_open')){
-    //     document.querySelector('.nav_right').className = "fixed"; 
+        document.querySelector('header').className = "fixed";  
     } else {
         document.querySelector('header').className = "";
     }
  };
 
 
- document.querySelector('.raccourci').onclick=function(e){
+document.querySelector('.raccourci').onclick=function(e){
     e.preventDefault();
     document.querySelector('.nav_right').classList.toggle('is_open');
 }
