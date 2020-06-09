22cd2ebcca6a6ef8ec51d9f8130a40a8e41575bf
Stan MELINE <stan.meline@gmail.com>
2020-06-09 16:59:05 +0200

    resize button
    Signed-off-by: Stan MELINE <stan.meline@gmail.com>

--- header.js
+++ header.js
@@ -1,5 +1,9 @@
 var rellax = new Rellax('.rellax', {center: true});
 window.onscroll = function () {
+    this.console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop > 100 && document.documentElement.scrollTop < 400) {
+        document.querySelector('but1').style.backgroundPosition="dqs";
+    }
     if (document.documentElement.scrollTop > 50) { 
         document.querySelector('header').className = "fixed"; 
     // } else if(document.documentElement.scrollTop > 50 && document.querySelector('.nav_right').className.contains('is_open')){
